8bec0c2dcdeb73204053eecd1b26d77af5c92555
alex-klim <goldencrane@ukr.net>
2017-11-09 12:42:40 +0200

    add channels-session

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,15 +1,25 @@
 from channels import Group
+from channels.sessions import channel_session
+from channels.auth import channel_session_user, channel_session_user_from_http
+from urllib.parse import parse_qs
+import json
 
 
+@channel_session_user_from_http
 def ws_add(message):
     message.reply_channel.send({"accept": True})
     Group("chat").add(message.reply_channel)
 
+@channel_session_user
 def ws_message(message):
-    print(message.items())
+    print(message.reply_channel.__dict__)
     Group("chat").send({
-        "text" : "[user] %s" % message.content['text'],
+        "text": json.dumps({
+            "text": message["text"],
+            "username": message.user.username,
+        }),
     })
 
+@channel_session_user
 def ws_disconnect(message):
     Group("chat").discard(message.reply_channel)
